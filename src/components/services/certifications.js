717cbf68ecb69dafb1001aab8246d58f75ec2fb0
Edmilson Constantino <edmilsonconstantino879@gmail.com>
2025-10-15 15:15:28 +0200

    ed

--- src/components/services/certifications.js
+++ src/components/services/certifications.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://127.0.0.1:8000"; 
+const BASE_URL = "https://cestificacoesiso-back.onrender.com"; 
 const API_URL = `${BASE_URL}/api/certifications/`;
 
 export default {
